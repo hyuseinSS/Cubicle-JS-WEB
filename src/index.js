f48e3d6d35d229d974baa9f1cf130cd7234af2c5
hyuseinSS <hyuseinss@gmail.com>
2023-12-23 21:37:22 +0000

    Render  main page successfully

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const handlebars = require("express-handlebars")
+const app = express();
+
+app.use("/static", express.static("public"))
+
+app.engine("hbs", handlebars.engine({
+    extname: "hbs",
+}))
+
+app.set("view engine", "hbs")
+app.set("views", "./src/views")
+
+app.get("/", (req, res) => {
+
+    res.render("index")
+})
+
+
+app.listen(5000, () => console.log("Server started on port 5000"))
